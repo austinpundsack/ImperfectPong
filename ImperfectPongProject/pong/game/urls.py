684d71ddddf6b3b566b16ebbf4ba6207510bdfac
MultipleScones <reid@midco.net>
2023-05-09 19:26:36 -0500

    thingbs

--- ImperfectPongProject/pong/game/urls.py
+++ ImperfectPongProject/pong/game/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from game import views
 
 urlpatterns = [
-    path('', views.instructions, name='instructions'),
+    path('', views.menu, name='menu'),
     path('game', views.game, name='game'),
     path('menu', views.menu, name='menu'),
+    path('instructions', views.instructions, name='instructions'),
 ]
